70c796e5037c36a004f4d4dcc745f387bfdc646b
AC-wrh <adolchristin.wrh@gmail.com>
2020-04-10 22:34:23 +0800

    Initial commit

--- System/Src/main.c
+++ System/Src/main.c
@@ -3,30 +3,30 @@
  * @version: 
  * @Author: Adol
  * @Date: 2020-04-09 16:25:14
- * @LastEditTime: 2020-04-10 17:25:53
+ * @LastEditTime: 2020-04-10 22:27:58
  */
-#include "main.h"
+#include "core.h"
 
 int main(void)
 {
-    unsigned char color_mode = 0;
+    // unsigned char color_mode = 0;
 
-    P3M1 = 0x80;
-    P3M0 = 0x80;
-    MS1 = 0;
-    send = 0;
+    // P3M1 = 0x80;
+    // P3M0 = 0x80;
+    // MS1 = 0;
+    // send = 0;
 
-    Init();
-    while (1) //Ö÷Ñ­»·¡£
-    {
-        if (send == 1)
-        {
-            TR0 = 0;
-            PWM_Dimming(color_mode, COLOR_CHANGE_INTERVAL_NORMAL, 25);
-            send = 0;
-            TR0 = 1;
-        }
-    }
+    // Init();
+    // while (1) //ï¿½ï¿½Ñ­ï¿½ï¿½ï¿½ï¿½
+    // {
+    //     if (send == 1)
+    //     {
+    //         TR0 = 0;
+    //         PWM_Dimming(color_mode, COLOR_CHANGE_INTERVAL_NORMAL, 25);
+    //         send = 0;
+    //         TR0 = 1;
+    //     }
+    // }
     
     return 0;
 }
